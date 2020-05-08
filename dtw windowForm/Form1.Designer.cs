07ba347efc8c98a30acb8834b2170c743637500b
znacks platinum <44054067+znacks@users.noreply.github.com>
2020-05-08 15:32:25 +0700

    Add files via upload

---
+++ dtw windowForm/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace dtw_windowForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableBox = new System.Windows.Forms.CheckBox();
+            this.graphBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(133, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(491, 24);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "1,1,2,4,3,5,3,2,3,2";
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.inputLabel.Location = new System.Drawing.Point(35, 35);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(82, 36);
+            this.inputLabel.TabIndex = 1;
+            this.inputLabel.Text = "Input";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Solution";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.solution_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(133, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 24);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1,2,4,3,5,3,2,3,2,5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.ColumnHeadersHeight = 10;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(868, 523);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(786, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 39);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(56, 652);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 36);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "- - -";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(890, 652);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 36);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "- - -";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(928, 134);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(938, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 36);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "- - -";
+            // 
+            // tableBox
+            // 
+            this.tableBox.AutoSize = true;
+            this.tableBox.Location = new System.Drawing.Point(944, 39);
+            this.tableBox.Name = "tableBox";
+            this.tableBox.Size = new System.Drawing.Size(66, 21);
+            this.tableBox.TabIndex = 16;
+            this.tableBox.Text = "Table";
+            this.tableBox.UseVisualStyleBackColor = true;
+            this.tableBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // graphBox
+            // 
+            this.graphBox.AutoSize = true;
+            this.graphBox.Location = new System.Drawing.Point(944, 63);
+            this.graphBox.Name = "graphBox";
+            this.graphBox.Size = new System.Drawing.Size(110, 21);
+            this.graphBox.TabIndex = 17;
+            this.graphBox.Text = "Graph < 400";
+            this.graphBox.UseVisualStyleBackColor = true;
+            this.graphBox.CheckedChanged += new System.EventHandler(this.graphBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1456, 718);
+            this.Controls.Add(this.graphBox);
+            this.Controls.Add(this.tableBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "DTW solution";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox tableBox;
+        private System.Windows.Forms.CheckBox graphBox;
+    }
+}
+
---
+++ dtw windowForm/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dtw_windowForm
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<string, float> dtw_matx = new Dictionary<string, float>();
+        static float[] addition;
+        static float[] mathX, mathY;
+        static Dictionary<string, float> dtw_row = new Dictionary<string, float>();
+        static bool tableEnable,graphEnable;
+        static float jx = -1, finalDistance, idk;
+
+        static DataTable dtwMatrixTable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static void DynamicTimeWarpingDistance(int i, int j)
+        {
+            float dist = Math.Abs(mathX[i] - mathY[j]);
+
+            // first char
+            if (j == 0 && i == 0)
+            {
+                dtw_matx.Add(key(0, 0), dist);
+            }
+            // first line
+            else if (j == 0)
+            {
+                dtw_matx.Add(key(i, 0), dist);
+                if (i != 0)
+                {
+                    addition = new float[]
+                    {
+                        dtw_matx[key(i-1,0)]
+                    };
+                }
+
+                dtw_matx[key(i, 0)] = dist + addition.Min();
+            }
+            //rest of the line
+            else if (j != 0)
+            {
+                dtw_matx.Add(key(i, j), dist);
+                if (i != 0)
+                {
+                    addition = new float[]
+                    {
+                        dtw_matx[key(i-1,j)],
+                        dtw_matx[key(i,j-1)],
+                        dtw_matx[key(i-1,j-1)],
+                    };
+                }
+                else
+                {
+                    addition = new float[]
+                    {
+                        dtw_matx[key(0,j-1)],
+                    };
+                }
+
+                dtw_matx[key(i, j)] = dist + addition.Min();
+            }
+
+            if (tableEnable)
+            {
+                if (jx != j)
+                {
+                    dtwMatrixTable.Columns.AddRange(new DataColumn[]
+                    {
+                        new DataColumn(j.ToString()+"j",typeof(int))
+                    });
+                }
+
+                while (idk < mathX.Length)
+                {
+                    dtwMatrixTable.Rows.Add();
+                    idk++;
+                }
+            }
+
+            jx = j;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            panel1.CreateGraphics().Clear(SystemColors.Control);
+
+            dtw_matx.Clear();
+            dtwMatrixTable.Columns.Clear();
+            dtwMatrixTable.Clear();
+            this.dataGridView1.Columns.Clear();
+
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.Refresh();
+
+            idk = 0;
+            finalDistance = 0;
+        }
+
+        float[] sampleSpace;
+        private void solution_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            FindPath();
+
+            float similarity;
+            try
+            {
+                similarity = (EuclidianDist() * 100) / (EuclidianDist() + finalDistance);
+            }
+            catch (DivideByZeroException)
+            {
+                similarity = 100;
+            }
+
+            label1.Text = "Final Distance : " + finalDistance.ToString();
+            label2.Text = "Similarity : " + similarity.ToString() + "%";
+
+            if (tableEnable) dataGridView1["0j", 0].Selected = false;
+        }
+
+        void FindPath()
+        {
+            string[] inputArray1 = textBox1.Text.Split(new Char[] { ',' });
+            string[] inputArray2 = textBox2.Text.Split(new Char[] { ',' });
+
+            mathX = Array.ConvertAll(inputArray1, s => float.TryParse(s, out var x) ? x : -1);
+            mathY = Array.ConvertAll(inputArray2, s => float.TryParse(s, out var x) ? x : -1);
+
+            for (int j = 0; j < mathY.Length; j++)
+            {
+                for (int i = 0; i < mathX.Length; i++)
+                {
+                    DynamicTimeWarpingDistance(i, j);
+                    this.dataGridView1.DataSource = dtwMatrixTable;
+                    if (tableEnable) dataGridView1[j.ToString() + "j", i].Value = dtw_matx[key(i, j)];
+
+                    PaintGraph(j, i, false);
+                }
+                if (tableEnable) dataGridView1.Columns[j.ToString() + "j"].Width = 25;
+            }
+
+            int jj = mathY.Length - 1, ii = mathX.Length - 1;
+
+            while (true)
+            {
+                if (tableEnable) { var selectedGrid = dataGridView1[jj.ToString() + "j", ii].Style.BackColor = Color.Yellow; }
+                PaintGraph(jj, ii,true);
+
+                finalDistance += dtw_matx[key(ii, jj)];
+
+                if (ii == 0 && jj == 0)
+                {
+                    sampleSpace = new float[]
+                    {
+                        dtw_matx[key(0,0)]
+                    };
+
+                    if (tableEnable) dataGridView1["0j", 0].Style.BackColor = Color.Yellow;
+
+                    break;
+                }
+                else if (ii == 0)
+                {
+                    sampleSpace = new float[]
+                    {
+                        dtw_matx[key(0,jj-1)]
+                    };
+                }
+                else if (jj == 0)
+                {
+                    sampleSpace = new float[]
+                    {
+                        dtw_matx[key(ii-1,0)]
+                    };
+                }
+                else
+                {
+                    sampleSpace = new float[]
+                    {
+                        dtw_matx[key(ii-1,jj)],
+                        dtw_matx[key(ii,jj-1)],
+                        dtw_matx[key(ii-1,jj-1)]
+                    };
+                }
+
+                var selectedIndex = sampleSpace.FindIndex(x => x == sampleSpace.Min());
+
+                if (ii == 0)
+                {
+                    jj -= 1;
+                }
+                else if (jj == 0)
+                {
+                    ii -= 1;
+                }
+                else
+                {
+                    if (selectedIndex == 2)
+                    {
+                        ii -= 1;
+                        jj -= 1;
+                    }
+                    else if (selectedIndex == 1)
+                    {
+                        jj -= 1;
+                    }
+                    else if (selectedIndex == 0)
+                    {
+                        ii -= 1;
+                    }
+                }
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            tableEnable = tableBox.Checked;
+        }
+
+        private void graphBox_CheckedChanged(object sender, EventArgs e)
+        {
+            graphEnable = graphBox.Checked;
+        }
+
+        void PaintGraph(int i,int j,bool highlight)
+        {
+            if (graphEnable)
+            {
+                Graphics gObject = panel1.CreateGraphics();
+
+                panel1.Width = 300;
+                panel1.Height = 300;
+
+                float stepX = panel1.Width / mathX.Length;
+                float stepY = panel1.Height / mathY.Length;
+
+                label3.Text = mathX.Length.ToString() + " x " + mathY.Length.ToString();
+
+                if (highlight) gObject.FillRectangle(new SolidBrush(Color.Red), stepX * i, stepY * j, stepX, stepY);
+                else gObject.FillRectangle(new SolidBrush(Color.White), stepX * i, stepY * j, stepX, stepY);
+            }
+        }
+
+        float EuclidianDist()
+        {
+            float euclidianDist = 0;
+            int ii = mathX.Length - 1, jj = mathY.Length - 1;
+
+            while(ii != 0 && jj != 0)
+            {
+                euclidianDist += Math.Abs(mathX[ii] - mathY[jj]);
+
+                ii--;
+                jj--;
+            }
+
+            return euclidianDist;
+        }
+
+        static string key(int i, int j)
+        {
+            string value = i.ToString() + "i " + j.ToString() + "j";
+
+            return value;
+        }
+    }
+    
+    static class ArrayExtensions
+    {
+        public static int FindIndex<T>(this T[] array, Predicate<T> match)
+        {
+            return Array.FindIndex(array, match);
+        }
+    }
+}
